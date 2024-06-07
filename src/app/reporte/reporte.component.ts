feb497aad13829bc90790f4a95396fe148368441
DevEfren <gilsonecruz92@gmail.com>
2024-06-07 01:49:23 -0500

    Se creo esquema para las tablas en branch gcruz

--- src/app/reporte/reporte.component.ts
+++ src/app/reporte/reporte.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuTablaComponent } from './menu-tabla/menu-tabla.component';
+import { TablaComponent } from './tabla/tabla.component';
 
 @Component({
   selector: 'app-reporte',
   standalone: true,
-  imports: [],
+  imports: [MenuTablaComponent,TablaComponent],
   templateUrl: './reporte.component.html',
   styleUrl: './reporte.component.scss'
 })
