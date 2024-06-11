ffce49a478df268141d7d15bd3e820c3ee15f1fc
SebitasMoreno <sebasmoresolz1@hotmail.com>
2024-06-11 04:06:46 -0600

    Update

--- src/app/administracion/administracion.component.ts
+++ src/app/administracion/administracion.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-administracion',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './administracion.component.html',
   styleUrl: './administracion.component.scss'
 })
