9d9e7cc0b3e6ebc918dcf677de4c28699bbf17b8
DevEfren <gilsonecruz92@gmail.com>
2024-06-17 21:56:54 -0500

    Se Optimizo el paginator y se mejoro la carga en tablets

--- src/app/reporte/tabla/tabla.component.ts
+++ src/app/reporte/tabla/tabla.component.ts
@@ -32,7 +32,7 @@ export class TablaComponent {
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
+          { title: 'Card 1', cols: 2, rows: 2 },
           { title: 'Card 2', cols: 1, rows: 1 },
           { title: 'Card 3', cols: 1, rows: 1 },
           { title: 'Card 4', cols: 1, rows: 1 }
