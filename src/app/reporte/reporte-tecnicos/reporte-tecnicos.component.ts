039e9500a3672daa979a1e64b6858112ef39e1b7
DevEfren <gilsonecruz92@gmail.com>
2024-06-26 00:48:58 -0500

    se actualizo campos de la tabla reporte

--- src/app/reporte/reporte-tecnicos/reporte-tecnicos.component.ts
+++ src/app/reporte/reporte-tecnicos/reporte-tecnicos.component.ts
@@ -1,49 +1,61 @@
 import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-reporte-tecnicos',
   standalone: true,
-  imports: [MatTableModule,MatPaginatorModule],
+  imports: [MatTableModule,MatPaginatorModule, DatePipe],
   templateUrl: './reporte-tecnicos.component.html',
   styleUrl: './reporte-tecnicos.component.scss'
 })
+
+
+
 export class ReporteTecnicosComponent implements AfterViewInit{
-  displayedColumns: string[] = ['IdTicket', 'UsResponsable', 'UsSolicitante', 'Estado'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  displayedColumns: string[] = ['fechaCreacion','fechaCierre', 'Ticket', 'Estado', 'tecnicoResponsable','categoria','solucion'];
+  dataSource = new MatTableDataSource<ReportePorTickets>(ELEMENT_DATA);
+ 
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    
   }
+
+  constructor() {
+    // Aquí puedes inicializar propiedades o realizar otras tareas necesarias
+  }
+  
+
+  
 }
-export interface PeriodicElement {
-  IdTicket: number;
-  UsResponsable: string;
-  UsSolicitante: number;
+export interface ReportePorTickets {
+  fechaCreacion:Date;
+  fechaCierre:Date;
+  Ticket: number;
   Estado: string;
+  tecnicoResponsable: string;
+  categoria: string;
+  solucion: string;
+ 
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {IdTicket: 1, UsResponsable: 'Hydrogen', UsSolicitante: 1.0079,  Estado: 'H'},
-  {IdTicket: 2, UsResponsable: 'Hbaque', UsSolicitante: 1.0079,  Estado: 'H'},
-  {IdTicket: 3, UsResponsable: 'pdoleen', UsSolicitante: 1.0079,  Estado: 'H'},
-  {IdTicket: 4, UsResponsable: 'Hydrogen', UsSolicitante: 1.0079,  Estado: 'H'},
-  {IdTicket: 5, UsResponsable: 'Hbaque', UsSolicitante: 1.0079,  Estado: 'H'},
-  {IdTicket: 6, UsResponsable: 'pdoleen', UsSolicitante: 1.0079,  Estado: 'H'},
-  {IdTicket: 7, UsResponsable: 'Hydrogen', UsSolicitante: 1.0079,  Estado: 'H'},
-  {IdTicket: 8, UsResponsable: 'Hbaque', UsSolicitante: 1.0079,  Estado: 'H'},
-  {IdTicket: 9, UsResponsable: 'pdoleen', UsSolicitante: 1.0079,  Estado: 'H'},
-  {IdTicket: 10, UsResponsable: 'Hydrogen', UsSolicitante: 1.0079,  Estado: 'H'},
-  {IdTicket: 11, UsResponsable: 'Hbaque', UsSolicitante: 1.0079,  Estado: 'H'},
-  {IdTicket: 12, UsResponsable: 'pdoleen', UsSolicitante: 1.0079,  Estado: 'H'},
-  {IdTicket: 13, UsResponsable: 'Hydrogen', UsSolicitante: 1.0079,  Estado: 'H'},
-  {IdTicket: 14, UsResponsable: 'Hbaque', UsSolicitante: 1.0079,  Estado: 'H'},
-  {IdTicket: 15, UsResponsable: 'pdoleen', UsSolicitante: 1.0079,  Estado: 'H'},
-  {IdTicket: 16, UsResponsable: 'Hydrogen', UsSolicitante: 1.0079,  Estado: 'H'},
-  {IdTicket: 17, UsResponsable: 'Hbaque', UsSolicitante: 1.0079,  Estado: 'H'},
-  {IdTicket: 18, UsResponsable: 'pdoleen', UsSolicitante: 1.0079,  Estado: 'H'},
+
+const ELEMENT_DATA: ReportePorTickets[] = [
+  {fechaCreacion: new Date(), fechaCierre: new Date(),Ticket : 2856,Estado: 'solucionado',tecnicoResponsable: 'GLOPEZ',categoria: 'redes',solucion:'se cambio router'},
+  {fechaCreacion: new Date(), fechaCierre: new Date(),Ticket : 2857,Estado: 'solucionado',tecnicoResponsable: 'GGOMEZ',categoria: 'redes',solucion:'se cambio router'},
+  {fechaCreacion: new Date(), fechaCierre: new Date(),Ticket : 2858,Estado: 'solucionado',tecnicoResponsable: 'LPAEZ',categoria: 'redes',solucion:'se cambio router'},
+  {fechaCreacion: new Date(), fechaCierre: new Date(),Ticket : 2859,Estado: 'solucionado',tecnicoResponsable: 'LPINCAY',categoria: 'redes',solucion:'se cambio router'},
+  {fechaCreacion: new Date(), fechaCierre: new Date(),Ticket : 2860,Estado: 'solucionado',tecnicoResponsable: 'TBAZURTO',categoria: 'HADWARE',solucion:'se cambio router'},
+  {fechaCreacion: new Date(), fechaCierre: new Date(),Ticket : 2861,Estado: 'solucionado',tecnicoResponsable: 'EVERA',categoria: 'redes',solucion:'se cambio router'},
+  {fechaCreacion: new Date(), fechaCierre: new Date(),Ticket : 2862,Estado: 'solucionado',tecnicoResponsable: 'NPEREZ',categoria: 'SOFTWARE',solucion:'se cambio router'},
+  {fechaCreacion: new Date(), fechaCierre: new Date(),Ticket : 2863,Estado: 'solucionado',tecnicoResponsable: 'LAMPUERO',categoria: 'redes',solucion:'se cambio router'},
+  {fechaCreacion: new Date(), fechaCierre: new Date(),Ticket : 2864,Estado: 'solucionado',tecnicoResponsable: 'GORTIZ',categoria: 'redes',solucion:'se cambio router'},
+  {fechaCreacion: new Date(), fechaCierre: new Date(),Ticket : 2865,Estado: 'solucionado',tecnicoResponsable: 'WSOLIS',categoria: 'SOFTWARE',solucion:'se cambio router'},
+  {fechaCreacion: new Date(), fechaCierre: new Date(),Ticket : 2866,Estado: 'solucionado',tecnicoResponsable: 'TZAMBRANO',categoria: 'HARDWARE',solucion:'se cambio router'},
+  {fechaCreacion: new Date(), fechaCierre: new Date(),Ticket : 2867,Estado: 'solucionado',tecnicoResponsable: 'YLOZANO',categoria: 'redes',solucion:'se cambio router'},
   
 ];
